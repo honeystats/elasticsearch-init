022157c20866770a6d12a3494cbd6c57ed5650c0
Kyle Sferrazza <git@kylesferrazza.com>
2022-03-12 14:49:48 -0500

    sets up ingest pipeline

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 
 	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esapi"
 	"github.com/sirupsen/logrus"
 )
 
@@ -32,9 +37,66 @@ func main() {
 			ELASTICSEARCH_URL,
 		},
 	}
-	_, err := elasticsearch.NewClient(cfg)
+	client, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		logrus.WithError(err).Fatalln("Error setting up ES client")
 	}
 	logrus.Info("Elasticsearch client initialized successfully.")
+
+	res, err := setupIngestPipelines(client)
+	if err != nil {
+		logrus.WithError(err).Fatalln("Error setting up ingest pipeline.")
+	}
+	logrus.WithField("result", res).Infoln("Succesfully set up ingest pipeline.")
+}
+
+func parseEsRes(resObj *esapi.Response, resErr error) ([]byte, error) {
+	defer resObj.Body.Close()
+
+	var out []byte
+	_, err := resObj.Body.Read(out)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Error reading ES response: %v", err))
+	}
+
+	if resObj.IsError() {
+		return nil, errors.New(fmt.Sprintf("Status text: [%s], Response text: [%s]", resObj.Status(), string(out)))
+	}
+
+	return out, nil
+}
+
+type IngestPipelineSetupReq struct {
+	Description string                    `json:"description"`
+	Processors  []IngestPipelineProcessor `json:"processors"`
+}
+
+type IngestPipelineProcessor struct {
+	GeoIP GeoIPProcessor `json:"geoip"`
+}
+type GeoIPProcessor struct {
+	Field string `json:"field"`
+}
+
+func setupIngestPipelines(client *elasticsearch.Client) (string, error) {
+	bod := IngestPipelineSetupReq{
+		Description: "GeoIP ingest",
+		Processors: []IngestPipelineProcessor{
+			{
+				GeoIP: GeoIPProcessor{
+					Field: "sourceIP",
+				},
+			},
+		},
+	}
+	byteArr, err := json.Marshal(bod)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("Error marshalling ES request: %v", err))
+	}
+	body := bytes.NewReader(byteArr)
+	txt, err := parseEsRes(client.Ingest.PutPipeline("geoip", body))
+	if err != nil {
+		return "", err
+	}
+	return string(txt), nil
 }
