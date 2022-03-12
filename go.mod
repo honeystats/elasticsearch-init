3fdf8966995db1ffc400e99502d1672fa8831e43
Kyle Sferrazza <git@kylesferrazza.com>
2022-03-12 12:55:15 -0500

    setup ES client

--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.17
 
 require github.com/sirupsen/logrus v1.8.1
 
-require golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+require github.com/elastic/go-elasticsearch/v8 v8.0.0
+
+require (
+	github.com/elastic/elastic-transport-go/v8 v8.0.0-alpha // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"os"
+
+	elasticsearch "github.com/elastic/go-elasticsearch/v8"
+	"github.com/sirupsen/logrus"
+)
+
+var ELASTICSEARCH_URL string
+
+func requireEnv(key string) string {
+	val, isSet := os.LookupEnv(key)
+	if !isSet {
+		logrus.Fatalf("Missing environment variable: [$%s]\n", key)
+	}
+	return val
+}
+
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+		PadLevelText:  true,
+	})
+	ELASTICSEARCH_URL = requireEnv("ELASTICSEARCH_URL")
+}
 
 func main() {
 	logrus.Info("Starting ES setup...")
+	_, err := elasticsearch.NewDefaultClient()
+	if err != nil {
+		logrus.WithError(err).Fatalln("Error setting up ES client")
+	}
+	logrus.Info("Elasticsearch client initialized successfully.")
 }
